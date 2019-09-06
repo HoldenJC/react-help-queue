1e695e1a150170478f9d5405d6ebe5096fdff826
HoldenJC <holdenj_c@yahoo.com>
2019-09-06 11:28:27 -0700

    add remaining homework additions for week 1 of React

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/images/queue-logo.png';
 
 function Header() {
+  var headerStyles = {
+    width: '200px'
+  };
   return (
     <div>
+      <img style={headerStyles} src={logo}/>
       <h1>Help Queue</h1>
       <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -32,13 +32,13 @@ module.exports = {
   module: {
     rules: [
       {
-      test: /\.jsx?$/,
-      enforce: "pre",
-      loader: "eslint-loader",
-      exclude: /node_modules/,
-      options: {
-        emitWarning: true,
-        configFile: "./.eslintrc.json"
+        test: /\.jsx?$/,
+        enforce: "pre",
+        loader: "eslint-loader",
+        exclude: /node_modules/,
+        options: {
+          emitWarning: true,
+          configFile: "./.eslintrc.json"
         }
       },
       {
@@ -54,6 +54,16 @@ module.exports = {
             "react-hot-loader/babel",
             "styled-jsx/babel"
           ]
+        }
+      },
+      {
+        test: /\.(png|gif|jp(e*)g|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8000,
+            name: 'images/[hash]-[name].[ext]'
+          }
         }
       }
     ]
