f9644b2f0c455f40e47c60dcd57aade320d858a5
HoldenJC <holdenj_c@yahoo.com>
2019-09-09 12:25:37 -0700

    follow along with weekend react hw

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import logo from '../assets/images/queue-logo.png';
 
 function Header() {
-  var headerStyles = {
+  var headerImageStyles = {
     width: '200px'
   };
   return (
     <div>
-      <img style={headerStyles} src={logo}/>
+      <img style={headerImageStyles} src={logo}/>
       <h1>Help Queue</h1>
       <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
     </div>
