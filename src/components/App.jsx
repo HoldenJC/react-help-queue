9fa1790157dd2cf7e528cdd580c3f6d335be4438
Student <example@example.com>
2019-09-12 14:19:59 -0700

    add more wednesday night hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,7 @@ class App extends React.Component {
       selectedTicket: null
     };
     this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+    this.handleChangingSelectedTicket = this.handleChangingSelectedTicket.bind(this);
   }
 
   componentDidMount() {
@@ -45,6 +46,10 @@ class App extends React.Component {
     this.setState({masterTicketList: newMasterTicketList});
   }
 
+  handleChangingSelectedTicket(ticket){
+      this.setState({selectedTicket: ticket});
+  }
+
   render(){
     return (
       <div>
@@ -52,7 +57,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
           <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
-          <Route path='/admin' render={(props)=><Admin ticketList={this.state.masterTicketList} currentRouterPath={props.location.pathname} />} />
+          <Route path='/admin' render={(props)=><Admin ticketList={this.state.masterTicketList} currentRouterPath={props.location.pathname} onTicketSelection={this.handleChangingSelectedTicket} selectedTicket={this.state.selectedTicket} />} />
           <Route component={Error404} />
         </Switch>
       </div>
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -7,12 +7,11 @@ function Ticket(props){
     <div>
       <h3>{props.location} - {props.names}</h3>
       <h4>{props.formattedWaitTime} ago</h4>
-      <p><em>{props.issue}</em></p>
       <hr/>
     </div>;
     if(props.currentRouterPath === '/admin') {
         return (
-            <div onClick={() => {alert('hey, you just clicked the ticket belonging to ' + props.name);}}>
+            <div onClick={() => {props.onTicketSelection({names: props.names, location: props.location, issue: props.issue, formattedWaitTime: props.formattedWaitTime});}}>
             {ticketInformation}
             </div>
         );
@@ -30,7 +29,8 @@ Ticket.propTypes = {
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
   formattedWaitTime: PropTypes.string.isRequired,
-  currentRouterPath: PropTypes.string
+  currentRouterPath: PropTypes.string,
+  onTicketSelection: PropTypes.func
 };
 
 export default Ticket;
